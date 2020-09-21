07cbd654cd55d4ab65d9e0860a6c4b3bc298c431
Craig McKeachie <craigdmckeachie@gmail.com>
2020-09-21 15:28:47 -0400

    commiting blank file for ease of starting

--- src/program.js
+++ src/program.js
@@ -1,10 +1,1 @@
-class Person {
-  first;
-  last;
-  constructor(first, last) {
-    this.first = first;
-    this.last = last;
-  }
-}
-let person = new Person('Craigss', 'McKeachie');
-console.log(person);
+
